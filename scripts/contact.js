89e8c0f0b019884e19e7bbeb5abba5201aac86b8
FrancoJRamos <fjr2950@email.vccs.edu>
2023-03-02 23:51:07 -0500

    3.2.23 Initial commit of contact.js

---
+++ scripts/contact.js
@@ -0,0 +1,46 @@
+/*  Shining Drive JS
+
+    Contact Page
+    Goals:
+        Toggle Visibility for divs
+        form validation
+    Author: Franco Ramos
+    Date:   03/02/2023
+
+    Filename: contact.js
+ */
+
+//Declare Variables/Grab Elements
+
+let resume = document.getElementById("resume_panel");
+let emailForm = document.getElementById("email_panel");
+let more = document.getElementById("more_panel");
+
+let panelButtons = document.getElementsByClassName("accordion");
+
+//set all panels to a closed behavior on load
+window.addEventListener("load", function(){
+    console.log("an anonymous function has been called on load");
+    resume.style.display = "none";
+    emailForm.style.display = "none";
+    more.style.display = "none";
+})
+
+//Test if a panel is displayed and toggle its state between block and hidden
+
+for (i=0; i<panelButtons.length; i++){
+    //console.log("this is the "+i+"th iteration" );
+    
+    panelButtons[i].addEventListener("click", function(){
+        //console.log(panelArray[1]);
+        //console.log(this.nextElementSibling);
+
+        //use an if else statement to toggle
+        if(this.nextElementSibling.style.display == "none"){
+            this.nextElementSibling.style.display = "block";
+        }
+        else{
+            this.nextElementSibling.style.display = "none";
+        }
+    })
+}
