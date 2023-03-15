d73712f377569bad00b9f2ec869fe524527f8adb
FrancoJRamos <fjr2950@email.vccs.edu>
2023-03-15 13:11:55 -0400

    3.13.23 Bio page 6th story update

--- scripts/bio.js
+++ scripts/bio.js
@@ -21,13 +21,13 @@ let textDisplay = document.getElementById("bio_story");
 
 let storyCount = 1;
 let storyUpdate = 0;
-let storyAlt = ["engineering", "carpentry", "uh-oh", "recovery", "asthma"]
+let storyAlt = ["Prologue","Engineering", "Carpentry", "Uh-Oh", "Recovery", "Pressure"]
 //story arrays - needed to store text in the stories, store in a seperate script page to save space/readability
 let storyButtons = document.querySelectorAll("input[name=story_select]");
 
 //create a function that changes the image to reflect the value of storyCount
-console.log(imageDisplay.childNodes);
-console.log(storyButtons);
+//console.log(imageDisplay.childNodes);
+//console.log(storyButtons);
 function imageUpdate(storyUpdate){
     imageDisplay.innerHTML = "<img src='images/bio/bio0"+storyUpdate+".png' alt='"+storyAlt[storyUpdate-1]+" image'>";
 }
@@ -36,9 +36,9 @@ function imageUpdate(storyUpdate){
 //remember that the 'rightArrow' Variable is technically a collection of all elements with the same class name
 //so the correct way to target that element on this page is as: 'rightArrow[0]'
 rightArrow[0].addEventListener("click", function(){
-    console.log("right arrow has been clicked");
-    //if statement initalizing storyCount to 1 if it goes over five
-    if (storyCount<5){
+    //console.log("right arrow has been clicked");
+    //if statement initalizing storyCount to 1 if it goes over six
+    if (storyCount<6){
         storyCount +=1;
     }
     else{
@@ -46,19 +46,21 @@ rightArrow[0].addEventListener("click", function(){
     }
     //console.log(storyCount);
     imageUpdate(storyCount);
+    textUpdate();
 })
 
 leftArrow[0].addEventListener("click", function(){
-    console.log("left arrow has been clicked");
-    //if statement initalizing storyCount to 5 if it goes under 1
+    //console.log("left arrow has been clicked");
+    //if statement initalizing storyCount to 6 if it goes under 1
     if (storyCount>1){
         storyCount -=1;
     }
     else{
-        storyCount=5;
+        storyCount=6;
     }
     //console.log(storyCount);
     imageUpdate(storyCount);
+    textUpdate();
 })
 
 /* replace current logic with that of object.checked like in chapter 3 lab 1
@@ -75,12 +77,23 @@ for (i=0; i<storyButtons.length; i++){
 for (let i=0; i<storyButtons.length; i++){
     //if a checkbox is checked, assign its value to storyCount and update the page
     storyButtons[i].addEventListener("change",function(){
-        console.log("radio button has been changed");
+        //console.log("radio button has been changed");
         if(storyButtons[i].checked){
             storyCount = Number(storyButtons[i].value)
-            console.log(storyCount);
+            //console.log(storyCount);
             imageUpdate(storyCount);
+            textUpdate();
         }
     })
     
+}
+
+//update story titles
+//console.log(textDisplay.childNodes);
+//console.log(textDisplay.childNodes[1]);
+//console.log(textDisplay.childNodes[1].innerHTML);
+
+function textUpdate(){
+    //replace the story title within the h3 element in textDisplay with its corresponding value in the storyAlt Array
+    textDisplay.childNodes[1].innerHTML=storyAlt[storyCount-1];
 }
